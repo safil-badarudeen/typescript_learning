73fb0ea7cef5018e7330bc702becd9b5a5a53805
safil badarudeen <safilbadarudeen@gmail.com>
2023-02-28 22:15:05 +0530

    added classws with private in ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,34 @@
-const hello:number = 3;
-
-console.log(hello)
+class Department {
+    private dep: string;
+    private employees: string[] = [];
+  
+    constructor(n: string) {
+      this.dep = n;
+    }
+  
+    describe(this: Department) {
+      console.log("Department :" + this.dep);
+    }
+  
+    addEmployee(employee:string){
+      this.employees.push(employee)
+    }
+  
+    printEmployeeInformation(){
+      console.log(this.employees.length);
+      console.log(this.employees)
+    }
+  }
+  
+  const DepartmentName = new Department("microbiology");
+  
+  //public aayath namukk ingane maatam
+  
+  // DepartmentName.dep = "hello" // ith maataan private aakam., private forces to write cleaner code
+  
+  DepartmentName.addEmployee("safil")
+  console.log(DepartmentName);
+  DepartmentName.printEmployeeInformation()
+  
+  
+  
